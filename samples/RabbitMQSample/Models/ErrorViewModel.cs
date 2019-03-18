7ca9c764b716c372c76317fd52ac2e71db77d1c6
毕殿卿 <bdq123@outlook.com>
2019-03-18 22:39:24 +0800

    +

---
+++ samples/RabbitMQSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMQSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
